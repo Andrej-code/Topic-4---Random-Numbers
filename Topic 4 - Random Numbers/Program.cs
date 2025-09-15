9feebe65d5757166c6f4995d9b7f603786ed3b4d
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-09-15 09:40:02 -0400

    Add project files.

---
+++ Topic 4 - Random Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Topic_4___Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // #1 Random Integers
+            Random generator = new Random();
+
+            int min, max;
+            Console.WriteLine("Enter a minimum and maximum number.");
+            Console.WriteLine();
+            Console.WriteLine("Minimum: ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Maximum: ");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine() ;
+            Console.WriteLine("Here are five random numbers.");
+            Console.WriteLine();
+            Console.WriteLine(generator.Next(min, max));
+            Console.WriteLine();
+            Console.WriteLine(generator.Next(min, max));
+            Console.WriteLine();
+            Console.WriteLine(generator.Next(min, max));
+            Console.WriteLine();
+            Console.WriteLine(generator.Next(min, max));
+            Console.WriteLine();
+            Console.WriteLine(generator.Next(min, max));
+            Console.WriteLine();
+        // #2 Dice Roller
+            
+        }
+    }
+}
